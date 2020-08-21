cdb548e3eca7582c0b2e53f5df4cff72492c886b
SGKwon95 <ssunder7@naver.com>
2020-08-21 18:14:27 +0900

    make calendar

---
+++ frontend/src/components/Layout.vue
@@ -0,0 +1,94 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar
+      app
+      clipped-right
+      color="blue"
+      shrink-on-scroll
+      dark
+
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title align="center"><div @click="home" align="center">My Calendar</div></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <slot name="menubar"></slot>
+    </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-item @click.stop="left = !left">
+          <v-list-item-action>
+            <v-icon>mdi-exit-to-app</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title @click="home">홈으로 이동</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-list dense>
+        <v-list-item @click.stop="left = !left">
+          <v-list-item-action>
+            <v-icon>mdi-exit-to-app</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title @click="login">로그인</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-list dense>
+        <v-list-item @click.stop="left = !left">
+          <v-list-item-action>
+            <div>>>></div>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title @click="suggest">추천 뉴스 보기</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-list dense>
+        <v-list-item @click.stop="left = !left">
+          <v-list-item-action>
+            <v-icon>mdi-exit-to-app</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title @click="savednews()">저장된 뉴스 보기</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-app>
+</template>
+
+<script>
+import router from '../router'
+
+export default {
+  data: () => ({
+    drawer: false,
+    left: false
+  }),
+  methods: {
+    home () {
+      (window.location.pathname !== '/') ? router.push('/') : router.go(0)
+    },
+    login () {
+      (window.location.pathname !== '/LoginPage') ? router.push('/LoginPage') : router.go(0)
+    },
+    suggest () {
+      (window.location.pathname !== '/suggestednews/list') ? router.push('/suggestednews/list') : router.go(0)
+    },
+    savednews () {
+      (window.location.pathname !== '/savednews/list') ? router.push('/savednews/list') : router.go(0)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
