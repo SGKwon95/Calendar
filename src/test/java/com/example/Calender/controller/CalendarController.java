6e44131ba6356dc260334b624037c34edc4d89db
SGKwon95 <ssunder7@naver.com>
2020-08-26 14:56:08 +0900

    db와 캘린더 연결

---
+++ src/test/java/com/example/Calender/controller/CalendarController.java
@@ -0,0 +1,31 @@
+package com.example.Calender.controller;
+
+import com.example.Calender.entity.Calendar;
+import com.example.Calender.service.CalendarService;
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Log
+@RestController
+@RequestMapping("/schedule")
+@CrossOrigin(origins = "http://localhost:8080", allowedHeaders = "*")
+public class CalendarController {
+    @Autowired
+    CalendarService service;
+
+    @GetMapping("")
+    public ResponseEntity<List<Calendar>> list() throws Exception {
+        log.info("등록된 스케줄 불러오기");
+
+        return new ResponseEntity<>(service.todoList(), HttpStatus.OK);
+    }
+
+}
